d6a2a081bfcffbe5c5f9f04ad65f857d33b754da
wail sari bey <wail.saribey@gmail.com>
2024-12-21 11:43:42 +0100

    added ev

--- src/GameUno/newGamePanel.java
+++ src/GameUno/newGamePanel.java
@@ -3,6 +3,8 @@ package GameUno;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -27,13 +29,20 @@ public class newGamePanel extends PanelGBLayout {
 		Button backMainBtn=new Button("Back To Main Menu");
 		Button soloGameBtn=new Button("Solo");
 		Button multyPlayer=new Button("MultiPlayer");
-		
+		JComboBox numberOfPlayers= new JComboBox(Choices);
+
 		// Setting Buttons Background Colors
 		backMainBtn.setBackground(Color.YELLOW);
 		soloGameBtn.setBackground(Color.LIGHT_GRAY);
 		multyPlayer.setBackground(Color.lightGray);
-		
-		JComboBox numberOfPlayers= new JComboBox(Choices);
+		// Setting Events for Btns
+		backMainBtn.addActionListener(
+				new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e){
+						BackEv();
+					}
+				});
 
 		// Adding Spacers
 		this.setGBC(0,0,3,1,1,1,GridBagConstraints.BOTH,GridBagConstraints.CENTER);
@@ -61,6 +70,15 @@ public class newGamePanel extends PanelGBLayout {
 		this.setGBC(1,3,1,1,1,1,GridBagConstraints.HORIZONTAL,GridBagConstraints.SOUTH);
 		this.add(numberOfPlayers,this.getGBC());
 		
+	}
+	void BackEv() {
+		if(this.getMainWindow() != null) 
+		{
+			this.getMainWindow().remove(this);
+			this.getMainWindow().add(new WelcomePanel(getMainWindow()));
+			this.getMainWindow().revalidate();
+			this.getMainWindow().repaint();
+		}
 	}
 
 }
